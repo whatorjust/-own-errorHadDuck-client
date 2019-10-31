f04157a09476af3912b622a6f387565c2b8a12c2
a <a@aui-MacBook-Pro.local>
2019-10-31 19:38:58 +0900

    :sparkles: set signsuccess component
    회원가입 성공 페이지

--- src/sign/SignSuccess.js
+++ src/sign/SignSuccess.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
 export default function SignSuccess() {
-  return <div>ㄴㅏ는 회원가입성공야</div>;
+  return (
+    <div>
+      <div>아이디</div>
+      <div>비밀번호</div>
+      <div>이메일</div>
+      <button>홈으로</button>
+    </div>
+  );
 }
