0b032594c40575f49f97a77543975f720b445edd
grin-quokka <grinquokka@gmail.com>
2019-10-31 22:27:25 +0900

    :art: singleview 컴포넌트 하나로 재사용
    signup컴포넌트 eslint 규칙 off, newErrorLog 컴포넌트 삭제

--- src/board/BoardThumbNail.js
+++ src/board/BoardThumbNail.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Nav from '../Nav';
 
 export default function BoardThumbNail() {
   return (
     <div>
+      <Nav />
       <h1>목록 (갯수)</h1>
       <ul>
         <li>글1</li>
--- src/board/SingleView.js
+++ src/board/SingleView.js
@@ -1,5 +1,48 @@
 import React from 'react';
+import Nav from '../Nav';
 
 export default function SingleView() {
-  return <div>ㄴㅏ는 싱글뷰야</div>;
+  // TODO: read ? update ? write?
+  return (
+    <div>
+      <Nav />
+
+      <div>
+        <h3>새로운 에러 작성</h3>
+        <div>
+          <span>제목</span>
+          <input type="text" />
+        </div>
+        <div>
+          <span>에러 코드</span>
+          <input type="text" />
+        </div>
+        <div>
+          <span>검색 키워드</span>
+          <input type="text" />
+          <button type="button">+</button>
+        </div>
+        <hr />
+        <div>
+          <div>
+            <span>참고한 페이지</span>
+            <input type="text" />
+          </div>
+          <div>
+            <span>이해한 내용</span>
+            <textArea />
+            <button type="button">+</button>
+          </div>
+        </div>
+        <hr />
+        <div>
+          <span>해결한 내용</span>
+          <textArea />
+        </div>
+      </div>
+
+      <button type="button">해결</button>
+      <button type="button">미해결</button>
+    </div>
+  );
 }
