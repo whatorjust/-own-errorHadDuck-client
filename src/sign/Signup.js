d261c0fa013bf994b59877b902fd67b4fcb37075
a <a@aui-MacBook-Pro.local>
2019-11-01 14:26:32 +0900

    :arrow_up: create signup axios
    axios 생성중

--- src/sign/Signup.js
+++ src/sign/Signup.js
@@ -1,6 +1,26 @@
 import React from 'react';
+import axios from 'axios';
 
 export default function Signup() {
+  const signupEndPoint = 'http://13.125.254.202:5000/users/signup';
+
+  function postData() {
+    return axios
+      .post(signupEndPoint, {
+        username: 'user',
+        password: '5555',
+        email: 'ghfflaktmxj@gmail.com'
+      })
+      .then(res => {
+        console.log(res);
+      });
+  }
+
+  function addUsersData() {
+    const arr = [];
+    arr.push();
+  }
+
   return (
     <div>
       <div>회원가입</div>
@@ -17,7 +37,9 @@ export default function Signup() {
         <input type="text" id="email" minLength="4" maxLength="8" size="10" />
       </p>
       <div>
-        <button type="button">제출</button>
+        <button onClick={postData} type="button">
+          제출
+        </button>
       </div>
     </div>
   );
