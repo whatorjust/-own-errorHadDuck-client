5f89b07e23262923079ba0103b5b6c01b0a579cb
grin-quokka <grinquokka@gmail.com>
2019-11-01 11:56:12 +0900

    :construction: handle changing input value
    id, pwd 입력시 state 변경

--- src/sign/Login.js
+++ src/sign/Login.js
@@ -1,21 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function Login() {
-  return (
-    <div>
-      <div>
-        <span>ID</span>
-        <input type="text" />
-      </div>
+// TODO: dotenv 적용
+// const path = require('path');
+// const dotenv = require('dotenv');
+const axios = require('axios');
 
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { idValue: '', pwdValue: '' };
+    this.handleLoginBtn = this.handleInputChange.bind(this);
+    this.handleLoginBtn = this.handleLoginBtn.bind(this);
+  }
+
+  handleInputChange = (e, stateKey) => {
+    this.setState({ [stateKey]: e.target.value });
+  };
+
+  render() {
+    const { idValue, pwdValue } = this.state;
+
+    return (
       <div>
-        <span>PWD</span>
-        <input type="text" />
+        <form onSubmit={this.handleSubmit}>
+          {' '}
+          <div>
+            <span>ID</span>
+            <input
+              value={idValue}
+              type="text"
+              onChange={e => this.handleInputChange(e, 'idValue')}
+            />
+          </div>
+          <div>
+            <span>PWD</span>
+            <input
+              value={pwdValue}
+              type="password"
+              onChange={e => this.handleInputChange(e, 'pwdValue')}
+            />
+          </div>
+          <div>
+            <button type="button" onClick={this.handleLoginBtn}>
+              로그인
+            </button>
+            <button type="button">회원가입</button>
+          </div>
+        </form>
       </div>
-      <div>
-        <button type="button">로그인</button>
-        <button type="button">회원가입</button>
-      </div>
-    </div>
-  );
+    );
+  }
 }
