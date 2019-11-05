36841873a296508729d185c1b11d461aaeded10a
grin-quokka <grinquokka@gmail.com>
2019-11-05 12:12:07 +0900

    :construction: save every text inputs
    버튼 클릭과 연동해서 인풋 텍스트 저장

--- src/board/SingleView.js
+++ src/board/SingleView.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-nested-ternary */
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 
@@ -6,18 +7,17 @@ const axios = require('axios');
 export default class SingleView extends Component {
   constructor(props) {
     super(props);
-    this.state = { mode: 'read', data: null, isErr: null };
+    this.state = {
+      mode: 'read',
+      data: null,
+      isErr: null,
+      writeKeyWords: [],
+      writeRefers: []
+    };
+    this.handleLeftBtn = this.handleLeftBtn.bind(this);
+    this.handleAddKeyword = this.handleAddKeyword.bind(this);
+    this.handleAddRef = this.handleAddRef.bind(this);
   }
-
-  // TODO:
-  /*
-  "/singleview/:postid" -> 1일때
-  this.props.match.params.postid; -> 없으면 mode : 'write'
-  -> 있으면 디폴트 읽기 모드 -> 수정버튼을 누르면 mode : 'update'
-  (읽기)수정/삭제 버튼 <-> 수정 완료/수정 취소 버튼 <-> 작성 완료/작성 취소 버튼
-  */
-
-  // 에러 코드 하이라이트? https://prismjs.com/
 
   componentDidMount() {
     const { match } = this.props;
@@ -48,12 +48,54 @@ export default class SingleView extends Component {
           }
         });
     } else {
-      // mode : write
+      this.setState({ mode: 'write' });
     }
   }
 
+  handleLeftBtn() {
+    const { mode } = this.state;
+    if (mode === 'write') {
+      console.log('code', this.writePostCode.value);
+      console.log('solve', this.writeSolution.value);
+    }
+  }
+
+  handleAddKeyword() {
+    const val = this.keyword.value;
+
+    if (val === '') return;
+
+    this.keyword.value = '';
+
+    this.setState(prev => {
+      return {
+        writeKeyWords: [...prev.writeKeyWords, <li>{val}</li>]
+      };
+    });
+  }
+
+  handleAddRef() {
+    const refUrlVal = this.refUrl.value;
+    const refUnderstandVal = this.refUnderstand.value;
+
+    if (refUrlVal === '' || refUnderstandVal === '') return;
+
+    this.refUrl.value = '';
+    this.refUnderstand.value = '';
+
+    this.setState(prev => {
+      return {
+        writeRefers: [...prev.writeRefers, { refUrlVal, refUnderstandVal }]
+      };
+    });
+  }
+
   render() {
-    const { data, isErr } = this.state;
+    const { data, isErr, mode, writeKeyWords, writeRefers } = this.state;
+    const leftBtn =
+      mode === 'read' ? '수정' : mode === 'write' ? '작성 완료' : '수정 완료';
+    const rightBtn =
+      mode === 'read' ? '삭제' : mode === 'write' ? '작성 취소' : '수정 취소';
 
     return (
       <div>
@@ -63,27 +105,87 @@ export default class SingleView extends Component {
           <div>
             <span>{data && data.iscomplete ? '해결' : '미해결'}</span>
           </div>
-          <h1>{data && data.postname}</h1>
+          <h1>
+            {data && data.postname}
+            {mode === 'write' && '새로운 오류 작성'}
+          </h1>
           <div>
             <span>에러 코드</span>
             <div>
               <pre>
                 <code>{data && data.postcode}</code>
               </pre>
+              {mode === 'write' && (
+                <textarea
+                  ref={element => {
+                    this.writePostCode = element;
+                  }}
+                />
+              )}
             </div>
           </div>
+
           <div>
             <span>검색 키워드</span>
+            {mode === 'write' && (
+              <div>
+                <input
+                  type="text"
+                  ref={element => {
+                    this.keyword = element;
+                  }}
+                />
+                <button type="button" onClick={this.handleAddKeyword}>
+                  +
+                </button>
+              </div>
+            )}
+
             <ul>
+              {writeKeyWords}
               {data &&
                 data.Poskeys.length !== 0 &&
                 data.Poskeys.map(ele => <li>{ele.Keyword.keyword}</li>)}
             </ul>
           </div>
           <hr />
+
           <div>
             <div>
               <span>참고한 페이지</span>
+              {mode === 'write' && (
+                <div>
+                  <button type="button" onClick={this.handleAddRef}>
+                    +
+                  </button>
+                  <ul>
+                    <li>
+                      url :{' '}
+                      <input
+                        type="text"
+                        ref={element => {
+                          this.refUrl = element;
+                        }}
+                      />
+                    </li>
+                    <li>
+                      이해한 내용 :{' '}
+                      <input
+                        type="text"
+                        ref={element => {
+                          this.refUnderstand = element;
+                        }}
+                      />
+                    </li>
+                    {writeRefers.map(ele => (
+                      <ul>
+                        <li>url : {ele.refUrlVal}</li>
+                        <li>이해한 내용 : {ele.refUnderstandVal}</li>
+                      </ul>
+                    ))}
+                  </ul>
+                </div>
+              )}
 
               {data &&
                 data.Refers.length !== 0 &&
@@ -96,74 +198,27 @@ export default class SingleView extends Component {
             </div>
           </div>
           <hr />
+
           <div>
             <span>해결한 내용</span>
-            <p>{data && data.solution}</p>
+            <p>
+              {data && data.solution}
+              {mode === 'write' && (
+                <textarea
+                  ref={element => {
+                    this.writeSolution = element;
+                  }}
+                />
+              )}
+            </p>
           </div>
         </div>
 
-        <button type="button">수정</button>
-        <button type="button">삭제</button>
+        <button type="button" onClick={this.handleLeftBtn}>
+          {leftBtn}
+        </button>
+        <button type="button">{rightBtn}</button>
       </div>
     );
   }
 }
-
-// [{
-//   id: 1,
-//   postname: '한글테스트',
-//   postcode: 'code1',
-//   solution: 'solution1',
-//   iscomplete: false,
-//   createdAt: '2019-11-03T08:42:36.000Z',
-//   updatedAt: '2019-11-03T08:42:36.000Z',
-//   UserId: 1,
-//   Poskeys: [
-//     {
-//       id: 1,
-//       poskey: null,
-//       createdAt: '2019-11-03T08:42:36.000Z',
-//       updatedAt: '2019-11-03T08:42:36.000Z',
-//       KeywordId: 1,
-//       PostId: 1,
-//       Keyword: {
-//         id: 1,
-//         keyword: '키워드',
-//         createdAt: '2019-11-03T08:42:36.000Z',
-//         updatedAt: '2019-11-03T08:42:36.000Z'
-//       }
-//     },
-//     {
-//       id: 2,
-//       poskey: null,
-//       createdAt: '2019-11-03T08:42:36.000Z',
-//       updatedAt: '2019-11-03T08:42:36.000Z',
-//       KeywordId: 2,
-//       PostId: 1,
-//       Keyword: {
-//         id: 2,
-//         keyword: 'kw2',
-//         createdAt: '2019-11-03T08:42:36.000Z',
-//         updatedAt: '2019-11-03T08:42:36.000Z'
-//       }
-//     }
-//   ],
-//   Refers: [
-//     {
-//       id: 1,
-//       referurl: 'naver.com',
-//       understand: '한글',
-//       createdAt: '2019-11-03T08:42:36.000Z',
-//       updatedAt: '2019-11-03T08:42:36.000Z',
-//       PostId: 1
-//     },
-//     {
-//       id: 2,
-//       referurl: 'daum.com',
-//       understand: 'what..?',
-//       createdAt: '2019-11-03T08:42:36.000Z',
-//       updatedAt: '2019-11-03T08:42:36.000Z',
-//       PostId: 1
-//     }
-//   ]
-// }];
