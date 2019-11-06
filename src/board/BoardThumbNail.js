ba861219f4aef2b5e7d2d8c550e4b4d955d54a20
a <a@aui-MacBook-Pro.local>
2019-11-06 22:50:45 +0900

    :triangular_flag_on_post: 오버뷰, 보드썸네일 완성
    오버뷰, 보드썸네일 완성

--- src/board/BoardThumbNail.js
+++ src/board/BoardThumbNail.js
@@ -1,15 +1,41 @@
 import React from 'react';
-import Nav from '../Nav';
+import { Link } from 'react-router-dom';
 
-export default function BoardThumbNail() {
+export default function BoardThumbNail(props) {
+  let routeJ = '';
+  let listHead = '';
+  const singleview = '/singleview/';
+  const { Data, type } = props;
+  const clearData = Data.map(ele => {
+    return ele.postname;
+  });
+
+  if (type === 'entire') {
+    routeJ = '/boardlist/entire';
+    listHead = '전체 목록';
+  } else if (type === 'incompleted') {
+    routeJ = '/boardlist/incompleted';
+    listHead = '미해결 목록';
+  } else {
+    routeJ = '/boardlist/completed';
+    listHead = '해결 목록';
+  }
   return (
-    <div>
-      <Nav />
-      <h1>목록 (갯수)</h1>
-      <ul>
-        <li>글1</li>
-        <li>글2</li>
-      </ul>
-    </div>
+    <p>
+      <div>
+        <div>
+          <Link to={routeJ}>{listHead} </Link>
+        </div>
+        <Link to={singleview + Data[0].id}>
+          <div>{clearData[0] || '최근 글이 없덕!'}</div>
+        </Link>
+        <Link to={clearData[1] ? singleview + Data[1].id : '/'}>
+          <div>{clearData[1] || '최근 글이 없덕!'}</div>
+        </Link>
+        <Link to={clearData[2] ? singleview + Data[2].id : '/overview'}>
+          <div>{clearData[2] || '최근 글이 없덕!'}</div>
+        </Link>
+      </div>
+    </p>
   );
 }
