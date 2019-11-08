e9e0c2bc091250d14ca08bdfd33bbd49ce03f8e5
grin-quokka <grinquokka@gmail.com>
2019-11-08 09:43:37 +0900

    :sparkles: 글 읽을 때 에러코드부분 하이라이트 기능

---
+++ src/board/CodeHighlight.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Prism from 'prismjs';
+import './prism.css';
+import { Row, Col } from 'antd';
+
+export default class CodeHighlight extends Component {
+  componentDidMount() {
+    setTimeout(() => Prism.highlightAll(), 0);
+  }
+
+  render() {
+    const { postcode } = this.props;
+
+    return (
+      <Row>
+        <Col span="20" offset={2}>
+          <pre>
+            <code className="language-html">{postcode}</code>
+          </pre>
+        </Col>
+      </Row>
+    );
+  }
+}
