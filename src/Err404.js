c10a5074dcbdc839edcef60d6b313da106828821
a <a@aui-MacBook-Pro.local>
2019-11-08 17:32:16 +0900

    :bug: fix login/logout access problem
    로그인/로그아웃 시에 페이지 직접접근 문제 해결

--- src/Err404.js
+++ src/Err404.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import errimg from './img/err404.jpg';
 
-export default function Err404() {
-  return (
-    <div>
-      <img src={errimg} alt="err page" style={{ height: '500px' }} />
-    </div>
-  );
+export default class Err404 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    setTimeout(this.handleNext, 5000);
+  }
+
+  handleNext = () => {
+    console.log(this.props);
+    const { history } = this.props;
+    history.push('/');
+  };
+
+  render() {
+    return (
+      <div>
+        <img src={errimg} alt="err page" style={{ height: '500px' }} />
+      </div>
+    );
+  }
 }
